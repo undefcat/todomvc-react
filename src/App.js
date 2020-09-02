59eb6686897e8cab0dfa2843dd53a8d166095bd9
김태훈 <rlaxognsk@plani.co.kr>
2020-09-02 09:28:04 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import Body from './Body'
+import Footer from './Footer'
+
+function App() {
+  return (
+    <section className="todoapp">
+      <Header />
+      <Body />
+      <Footer />
+    </section>
+  )
+}
+
+export default App
